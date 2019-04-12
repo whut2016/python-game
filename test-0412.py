4a644c71bf6e95064f79ed2a1f775a35dc64d4a2
whut2016 <2436038515@qq.com>
2019-04-13 01:42:16 +0800

    fix:add new test code

--- test-0412.py
+++ test-0412.py
@@ -7,3 +7,21 @@ print 'python is beginning'
 a = 04
 b = 12
 print 'start day:', a ,'-',b
+
+def main():
+    print '############################'
+    print 'fuck oneconnect'
+
+
+main()
+
+print '############################'
+
+def main_test(length,size):
+    print 'fieldName',':',length,'fieldVaule',':',size
+ßß
+main_test(1234, 10)
+
+print '############################'
+
+
