7a4e13be69b8e1b0b6aacf18fd9146fc033f6c9c
whut2016 <2436038515@qq.com>
2019-04-13 13:35:37 +0800

    fix:add new file

--- test-0412.py
+++ test-0412.py
@@ -19,9 +19,29 @@ print '############################'
 
 def main_test(length,size):
     print 'fieldName',':',length,'fieldVaule',':',size
-ßß
+
 main_test(1234, 10)
 
 print '############################'
 
+#list
+#tuple
+#dictionary---like chain,not a list
 
+dict = {}
+dict['2'] = ['test2']
+dict['3'] = ['test3']
+print 'first output'
+print dict.keys()
+print dict.values()
+print 'second output'
+print dict.keys()
+print dict.values()
+
+
+# or and not /logistic numbers
+
+# if for while
+
+import math
+print math.sqrt(9)
